0b6ec7479a84da8c09e5bb5ecab2d3962592b7cd
rogerchanac007 <armandoroger007@gmail.com>
2023-03-12 19:14:16 +0000

    Agregando el formulario de creacion

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name="post_list"),
+    path('post_new/', views.post_new, name="post_new"),
     path('blog/<pk>', views.post_detail, name="post_detail"),
 ]
