5c719cbaa9968bec5b0d412878a65b54464c2c49
rogerchanac007 <armandoroger007@gmail.com>
2023-03-12 19:48:50 +0000

    Redisenando la pagina web

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name="post_list"),
     path('post_new/', views.post_new, name="post_new"),
+    path('post/<pk>/edit', views.post_edit, name="post_edit"),
     path('blog/<pk>', views.post_detail, name="post_detail"),
 ]
